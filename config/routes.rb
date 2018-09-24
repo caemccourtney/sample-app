4d366a29846f707aa709f7b35f612610030d6320
caemccourtney <carlos.edu.mcc@gmail.com>
2018-09-24 10:47:07 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -7,6 +10,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  
 
 end
